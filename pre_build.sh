2111bebdc243853d5a18f0db59f78e1d4bf1100f
jakob <jakobschuster0@gmail.com>
2024-01-16 16:02:54 +1100

    updated libz_static thing

--- pre_build.sh
+++ pre_build.sh
@@ -2,4 +2,4 @@
 
 echo "Running pre-build script"
 
-sed '/^export CXX.*/a export PATH="/opt/osxcross/target/bin:$PATH"\nexport LIBZ_SYS_STATIC=1' -i /build.sh
+sed '/^export LIBZ_SYS_STATIC=1' -i /build.sh
