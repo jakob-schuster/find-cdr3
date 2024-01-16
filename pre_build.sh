33e321868dd1af660f867107bd79fa947cbe650a
jakob <jakobschuster0@gmail.com>
2024-01-16 16:12:02 +1100

    changed around the workflow again

--- pre_build.sh
+++ pre_build.sh
@@ -1,5 +1,22 @@
-#!/bin/sh
+# ... snip ...
 
-echo "Running pre-build script"
+MACOS_TARGET="x86_64-apple-darwin"
 
-sed '/^export CXX.*/aexport LIBZ_SYS_STATIC=1' -i /build.sh
+echo "Building target for platform ${MACOS_TARGET}"
+echo
+
+# Add osxcross toolchain to path
+export PATH="$(pwd)/osxcross/target/bin:$PATH"
+
+# Make libz-sys (git2-rs -> libgit2-sys -> libz-sys) build as a statically linked lib
+# This prevents the host zlib from being linked
+export LIBZ_SYS_STATIC=1
+
+# Use Clang for C/C++ builds
+export CC=o64-clang
+export CXX=o64-clang++
+
+cargo build --release --target "${MACOS_TARGET}"
+
+echo
+echo Done
